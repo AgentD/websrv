be028b45d83eaa6d21a7b6a848a8e11606b523c9
David Oberhollenzer <david.oberhollenzer@tele2.at>
2016-06-12 14:52:16 +0200

    Rework reverse proxy implementation
     - Write request to string and send to receiving end at once
     - Don't alter original request
    
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@tele2.at>

--- http/proxy.c
+++ http/proxy.c
@@ -14,9 +14,11 @@
 #ifdef HAVE_PROXY
 static const char* mustencode = "!#$&'()*+,/:;=?@[]";
 
-static void write_url_encoded( int fd, const unsigned char* str, int ispath )
+static int write_url_encoded( string* out, const unsigned char* str,
+                              int ispath )
 {
-    int i;
+    char hexbuf[16];
+    int i, ret = 1;
 
     for( i=0; str[i]!=0; ++i )
     {
@@ -26,9 +28,10 @@ static void write_url_encoded( int fd, const unsigned char* str, int ispath )
         if( (str[i] & 0x80) || strchr( mustencode, str[i] ) )
         {
             if( i )
-                write( fd, str, i );
+                ret = ret && string_append_len( out, (const char*)str, i );
 
-            dprintf( fd, "%%%02X", (int)str[i] );
+            sprintf( hexbuf, "%%%02X", (int)str[i] );
+            ret = ret && string_append_len( out, hexbuf, 4 );
 
             str = str + i + 1;
             i = -1;
@@ -36,82 +39,96 @@ static void write_url_encoded( int fd, const unsigned char* str, int ispath )
     }
 
     if( i )
-        write( fd, str, i );
+        ret = ret && string_append_len( out, (const char*)str, i );
+    return ret;
 }
 
-static void write_encoded_list( int fd, const char* field, const char* list,
-                                int count, char sep )
+static int write_encoded_list( string* out, const char* field,
+                               const char* list, int count, char sep )
 {
     const char* ptr;
-    int i;
+    int i, ret = 1;
 
-    write( fd, field, strlen(field) );
+    ret = ret && string_append( out, field );
     ptr = list;
 
     for( i=0; i<count; ++i )
     {
-        write_url_encoded( fd, (const unsigned char*)ptr, 0 );
+        ret = ret && write_url_encoded( out, (const unsigned char*)ptr, 0 );
         ptr += strlen(ptr);
-        write( fd, "=", 1 );
-        write_url_encoded( fd, (const unsigned char*)ptr, 0 );
+        ret = ret && string_append_len( out, "=", 1 );
+        ret = ret && write_url_encoded( out, (const unsigned char*)ptr, 0 );
         ptr += strlen(ptr);
 
         if( (i+1) < count )
-            write( fd, &sep, 1 );
+            ret = ret && string_append_len( out, &sep, 1 );
     }
+    return ret;
 }
 
-static int forward_request( int fd, const char* addr, http_request* req )
+static int append_field( string* out, const char* field, const char* value )
 {
-    int fwd, pfd[2], ret = 0;
-    size_t i, size, pipedata;
+    int ret = 1;
+    ret = ret && string_append( out, field );
+    ret = ret && string_append( out, value );
+    ret = ret && string_append( out, "\r\n" );
+    return ret;
+}
+
+static int request_to_string( string* str, const http_request* req,
+                              const char* path )
+{
     const char* ptr;
-    char temp[512];
+    int i, ret = 1;
+    char temp[64];
     struct tm stm;
     time_t t;
 
-    if( pipe(pfd) != 0 )
-    {
-        CRITICAL("pipe: %s", strerror(errno));
-        return 0;
-    }
-
-    fwd = connect_to( addr, 0, AF_UNIX );
-
-    if( fwd < 0 )
-        goto out_pipe;
-
-    /* generate request */
-    dprintf( fwd, "%s/", http_method_to_string(req->method) );
-    write_url_encoded( fwd, (const unsigned char*)req->path, 1 );
+    ret = ret && string_append( str, http_method_to_string(req->method) );
+    ret = ret && string_append_len( str, "/", 1 );
+    ret = ret && write_url_encoded( str, (const unsigned char*)path, 1 );
 
     if( req->getargs && req->numargs )
-        write_encoded_list( fwd, "?", req->getargs, req->numargs, '&' );
+    {
+        ret = ret && write_encoded_list( str, "?", req->getargs,
+                                         req->numargs, '&' );
+    }
 
-    dprintf( fwd, " HTTP/1.0\r\n" );
+    ret = ret && string_append( str, " HTTP/1.0\r\n" );
 
-    if( req->host   ) dprintf( fwd, "Host: %s\r\n",            req->host   );
-    if( req->type   ) dprintf( fwd, "Content-Type: %s\r\n",    req->type   );
-    if( req->length ) dprintf( fwd, "Content-Length: %lu\r\n", req->length );
+    if( req->host   ) ret = ret&&append_field(str,"Host: ",req->host);
+    if( req->type   ) ret = ret&&append_field(str,"Content-Type: ",req->type);
+    if( req->length )
+    {
+        sprintf( temp, "%lu", req->length );
+        ret = ret && append_field( str, "Content-Length: ", temp );
+    }
 
     if( req->ifmod )
     {
         t = req->ifmod;
         localtime_r( &t, &stm );
         strftime( temp, sizeof(temp), "%a, %d %b %Y %H:%M:%S %Z", &stm );
-        dprintf( fwd, "If-Modified-Since: %s\r\n", temp );
+        ret = ret && append_field( str, "If-Modified-Since: ", temp );
     }
 
     if( req->accept & (ENC_DEFLATE|ENC_GZIP) )
     {
-        i = req->accept;
+        i = req->accept & (ENC_DEFLATE|ENC_GZIP);
         temp[0] = 0;
 
-        if( i & ENC_DEFLATE ) { strcat(temp, "deflate"); i &= ~ENC_DEFLATE; }
-        if( i               )   strcat( temp, ", " );
-        if( i & ENC_GZIP    ) { strcat(temp, "gzip");    i &= ~ENC_GZIP;    }
+        if( i & ENC_DEFLATE )
+        {
+            strcat( temp, "deflate" );
+            i &= ~ENC_DEFLATE;
+            if( i )
+                strcat( temp, ", " );
+        }
 
-        dprintf( fwd, "Accept-Encoding: %s\r\n", temp );
+        if( i & ENC_GZIP )
+            strcat( temp, "gzip" );
+
+        ret = ret && append_field( str, "Accept-Encoding: ", temp );
     }
 
     switch( req->encoding )
@@ -122,23 +139,48 @@ static int forward_request( int fd, const char* addr, http_request* req )
     }
 
     if( ptr )
-        dprintf( fwd, "Content-Encoding: %s\r\n", ptr );
+        ret = ret && append_field( str, "Content-Encoding: ", ptr );
 
     if( req->cookies && req->numcookies )
     {
-        write_encoded_list( fwd, "Cookie: ", req->cookies,
-                            req->numcookies, ';' );
+        ret = ret && write_encoded_list( str, "Cookie: ", req->cookies,
+                                         req->numcookies, ';' );
     }
 
-    dprintf( fwd, "Connection: Close\r\n\r\n" );
+    ret = ret && string_append( str, "Connection: close\r\n\r\n" );
+    return ret;
+}
 
-    /* forward request content */
-    splice_to_sock( pfd, fd, fwd, req->length, 0 );
+static int forward_request( int fd, const char* addr,
+                            const http_request* req, const char* path )
+{
+    size_t size = 0, pipedata = 0;
+    int fwd, pfd[2], ret = 0;
+    char temp[512];
+    string str;
+
+    /* generate request */
+    if( !string_init( &str ) )
+        return 0;
+    if( !request_to_string( &str, req, path ) )
+        goto out_str;
+
+    /* forward request */
+    if( pipe(pfd) != 0 )
+    {
+        CRITICAL("pipe: %s", strerror(errno));
+        goto out_str;
+    }
+
+    write( pfd[1], str.data, str.used );
+
+    fwd = connect_to( addr, 0, AF_UNIX );
+    if( fwd < 0 )
+        goto out_pipe;
+
+    splice_to_sock( pfd, fd, fwd, req->length, str.used );
 
     /* load HTTP response into pipe and substitute some header fields */
-    pipedata = 0;
-    size = 0;
-
     do
     {
         if( !read_line( fwd, temp, sizeof(temp) ) )
@@ -157,9 +199,6 @@ static int forward_request( int fd, const char* addr, http_request* req )
     }
     while( strlen(temp) );
 
-    /* make sure remote fails if it tries to force keep-alive */
-    shutdown( fwd, SHUT_WR );
-
     /* forward response content */
     splice_to_sock( pfd, fwd, fd, size, pipedata );
     ret = 1;
@@ -168,10 +207,12 @@ out:
 out_pipe:
     close( pfd[0] );
     close( pfd[1] );
+out_str:
+    string_cleanup( &str );
     return ret;
 }
 
-int proxy_handle_request( int fd, const cfg_host* h, http_request* req )
+int proxy_handle_request( int fd, const cfg_host* h, const http_request* req )
 {
     size_t len = strlen(h->proxydir);
     const char* ptr;
@@ -185,8 +226,7 @@ int proxy_handle_request( int fd, const cfg_host* h, http_request* req )
 
     INFO("Forwarding /%s to %s:/%s", req->path, h->proxysock, ptr);
 
-    req->path = ptr;
-    if( !forward_request( fd, h->proxysock, req ) )
+    if( !forward_request( fd, h->proxysock, req, ptr ) )
         return ERR_INTERNAL;
 
     return 0;
--- http/proxy.h
+++ http/proxy.h
@@ -4,7 +4,7 @@
 #include "http.h"
 #include "conf.h"
 
-int proxy_handle_request( int fd, const cfg_host* h, http_request* req );
+int proxy_handle_request(int fd, const cfg_host* h, const http_request* req);
 
 #endif /* PROXY_H */
 

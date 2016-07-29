c3fa9ef7be43621aca4fa4d2378708da3a9d3125
David Oberhollenzer <david.oberhollenzer@tele2.at>
2016-07-29 12:43:15 +0200

    Yet another logging cleanup
     - Fail if log-file open fails
     - Lock the actual log text print, so we don't get a mishmash
       of different log lines from different processes
     - Print PID so we can identify different keep-alive request streams
    
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@tele2.at>

--- common/log.c
+++ common/log.c
@@ -1,31 +1,47 @@
+#include <sys/eventfd.h>
 #include <execinfo.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include <string.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <errno.h>
 #include <time.h>
 
 #include "log.h"
 
 static int max_level = LEVEL_WARNING;
+static int lockfd = -1;
 
 int log_init( const char* file, int level )
 {
     int fd;
 
+    lockfd = eventfd(1, EFD_CLOEXEC|EFD_SEMAPHORE);
+    if( lockfd < 0 )
+        return 0;
+
     if( file )
     {
         fd = open(file,O_WRONLY|O_APPEND|O_CREAT|O_SYNC|O_CLOEXEC, 0600);
+        if( fd < 0 )
+            goto fail;
 
         if( fd < 0 || dup2( fd, STDERR_FILENO ) < 0 )
+        {
             perror( file );
+            goto fail;
+        }
 
         close( fd );
     }
 
     max_level = level;
     return 1;
+fail:
+    close( lockfd );
+    return 0;
 }
 
 void log_printf( int level, const char* fmt, ... )
@@ -33,6 +49,7 @@ void log_printf( int level, const char* fmt, ... )
     const char* lvstr;
     char temp[128];
     struct tm stm;
+    uint64_t val;
     va_list ap;
     time_t t;
 
@@ -52,9 +69,19 @@ void log_printf( int level, const char* fmt, ... )
     strftime( temp, sizeof(temp), "[%Y-%m-%d %H:%M:%S %Z]", &stm );
 
     va_start( ap, fmt );
-    fprintf(stderr, "%s%s ", temp, lvstr);
+
+    /* lock */
+    read(lockfd, &val, 8);
+
+    /* generate log line */
+    fprintf(stderr, "%s[%d]%s ", temp, getpid(), lvstr);
     vfprintf(stderr, fmt, ap);
     fputc('\n', stderr);
+
+    /* unlock */
+    val = 1;
+    write(lockfd, &val, 8);
+
     va_end( ap );
 }
 
@@ -63,20 +90,31 @@ void print_stacktrace( void )
     void *buffer[100];
     char **strings;
     int j, nptrs;
+    uint64_t val;
+    pid_t pid;
 
     nptrs = backtrace( buffer, sizeof(buffer)/sizeof(buffer[0]) );
     strings = backtrace_symbols( buffer, nptrs );
+    pid = getpid();
 
+    /* lock */
+    read(lockfd, &val, 8);
+
+    /* print stack trace */
     if( strings )
     {
         for( j = 0; j < nptrs; ++j )
-            fprintf( stderr, "%s\n", strings[j] );
-
-        free( strings );
+            fprintf( stderr, "[%d] %s\n", pid, strings[j] );
     }
     else
     {
-        perror( "backtrace_symbols" );
+        fprintf(stderr, "[%d] backtrace_symbols: %s\n", pid, strerror(errno));
     }
+
+    /* unlock */
+    val = 1;
+    write(lockfd, &val, 8);
+
+    free( strings );
 }
 
